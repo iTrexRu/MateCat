62dc65ddf6209e3ae943dabef2e0d0d3314d682c
iTrexRu <32538246+iTrexRu@users.noreply.github.com>
2025-06-16 00:12:08 +0300

    Update Bootstrap.php
    добавили временную отладку
        ini_set('display_errors', true);
        ini_set('error_log', self::$_ROOT . '/local_storage/log_archive/php_errors.txt');
        error_reporting(E_ALL);

--- inc/Bootstrap.php
+++ inc/Bootstrap.php
@@ -26,7 +26,10 @@ class Bootstrap {
     }
 
     private function __construct( SplFileInfo $config_file = null, SplFileInfo $task_runner_config_file = null ) {
-
+    ini_set('display_errors', true);
+    ini_set('error_log', self::$_ROOT . '/local_storage/log_archive/php_errors.txt');
+    error_reporting(E_ALL);
+        
         self::$_ROOT = realpath( dirname( __FILE__ ) . '/../' );
 
         if ( $config_file != null ) {
